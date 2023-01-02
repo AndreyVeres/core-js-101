276e5b6944605bf744eb8fc5ed70b1aa8c0a52d1
Andrey <spacecmetanka@gmail.com>
2023-01-02 17:14:15 +0300

    object tasks

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -58,10 +58,12 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  // throw new Error('Not implemented');
+  const object = JSON.parse(json);
+  Object.setPrototypeOf(object, proto);
+  return object;
 }
-
 
 /**
  * Css selectors builder
